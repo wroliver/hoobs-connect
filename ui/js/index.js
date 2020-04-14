0131b0e52e30af074736b6ca77eebface7318ed1
Michael Kellsy <mkellsy@gmail.com>
2020-04-13 18:17:03 -0600

    fix material network select

--- ui/js/index.js
+++ ui/js/index.js
@@ -2,31 +2,30 @@ $(function(){
     const form = $("#connect-form");
     const select = $("#ssid-select");
 
-	let networks = undefined;
+    let networks = undefined;
 
-	function showHideEnterpriseSettings() {
-		const security = $(this).find(":selected").attr("data-security");
+    function showHideEnterpriseSettings() {
+        const security = $(this).find(":selected").attr("data-security");
 
-		if(security === "enterprise") {
-			$("#identity-group").show();
-		} else {
-			$("#identity-group").hide();
-		}
+        if(security === "enterprise") {
+            $("#identity-group").show();
+        } else {
+            $("#identity-group").hide();
+        }
     }
     
-    select.formSelect();
-	select.change(showHideEnterpriseSettings);
+    select.change(showHideEnterpriseSettings);
 
-	$.get("/networks", (data) => {
-		if(data.length === 0){
-			$(".before-submit").hide();
-			$("#no-networks-message").removeClass("hidden");
-		} else {
+    $.get("/networks", (data) => {
+        if(data.length === 0){
+            $(".before-submit").hide();
+            $("#no-networks-message").removeClass("hidden");
+        } else {
             networks = JSON.parse(data);
 
             const ssids = [];
 
-			$.each(networks, (_index, value) => {
+            $.each(networks, (_index, value) => {
                 if (ssids.indexOf(value.ssid) === -1) {
                     select.append(
                         $("<option>").text(value.ssid).attr("value", value.ssid).attr("data-security", value.security)
@@ -34,18 +33,20 @@ $(function(){
 
                     ssids.push(value.ssid);
                 }
-			});
+            });
 
-			jQuery.proxy(showHideEnterpriseSettings, select)();
-		}
-	});
+            jQuery.proxy(showHideEnterpriseSettings, select)();
+        }
+    }).always(() => {
+        select.formSelect();
+    });
 
-	form.submit((event) => {
-		event.preventDefault();
+    form.submit((event) => {
+        event.preventDefault();
 
-		$.post("/connect", form.serialize(), () => {
-			$(".before-submit").hide();
-			$("#submit-message").removeClass("hidden");
-		});
-	});
+        $.post("/connect", form.serialize(), () => {
+            $(".before-submit").hide();
+            $("#submit-message").removeClass("hidden");
+        });
+    });
 });
